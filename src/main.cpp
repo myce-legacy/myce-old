0152ec58950615ffca3ae16fa3a513d1c0154e7b
John <ComputerCraftr@users.noreply.github.com>
2018-08-17 20:59:03 -0700

    Revert transaction checks

--- src/main.cpp
+++ src/main.cpp
@@ -1073,9 +1073,9 @@ bool CheckTransaction(const CTransaction& tx, bool fZerocoinActive, bool fReject
     // Some primitive replay protection for future use
     if (tx.nVersion > 4)
         return state.DoS(50, error("CheckTransaction() : unknown transaction version"), REJECT_INVALID, "bad-txns-version");
-    else if (tx.nVersion < 3 && chainActive.Height()+1 >= Params().WALLET_UPGRADE_BLOCK())
+    else if (tx.nVersion < 3 && chainActive.Height() >= Params().WALLET_UPGRADE_BLOCK())
         return state.DoS(50, error("CheckTransaction() : transaction version must be at least 3 after upgrade block"), REJECT_INVALID, "bad-txns-version");
-    else if (tx.nVersion > 2 && chainActive.Height()+1 < Params().WALLET_UPGRADE_BLOCK())
+    else if (tx.nVersion > 2 && chainActive.Height() < Params().WALLET_UPGRADE_BLOCK())
         return state.DoS(50, error("CheckTransaction() : transaction version must be under 2 before upgrade block"), REJECT_INVALID, "bad-txns-version");
 
     // Size limits
