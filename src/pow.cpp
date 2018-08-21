ddff991a88a8198478fe5d223fa165bf86ca66c5
John <ComputerCraftr@users.noreply.github.com>
2018-08-21 12:40:45 -0700

    Continue testing

--- src/pow.cpp
+++ src/pow.cpp
@@ -66,8 +66,10 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         bnNew *= ((nInterval - 1) * nTargetSpacing + nActualSpacing + nActualSpacing);
         bnNew /= ((nInterval + 1) * nTargetSpacing);
 
-        if (pindexLast->nHeight < (Params().WALLET_UPGRADE_BLOCK()+8) && pindexLast->nHeight+2 >= Params().WALLET_UPGRADE_BLOCK())
-            bnNew /= (int)pow(4.0, (double)(8+Params().WALLET_UPGRADE_BLOCK()-pindexLast->nHeight)); // slash difficulty and gradually ramp back up over 10 blocks
+        int height = pindexLast->nHeight + 1;
+
+        if (height < (Params().WALLET_UPGRADE_BLOCK()+10) && height >= Params().WALLET_UPGRADE_BLOCK())
+            bnNew /= (int)pow(4.0, (double)(10+Params().WALLET_UPGRADE_BLOCK()-height)); // slash difficulty and gradually ramp back up over 10 blocks
 
         if (bnNew <= 0 || bnNew > bnTargetLimit)
             bnNew = bnTargetLimit;
