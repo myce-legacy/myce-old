cdc22fe37aa945588df191ddde052c9a4c3a05b4
apaulb <39216004+apaulb@users.noreply.github.com>
2018-08-23 17:51:06 +0200

    Update fork block and reduce difficulty for testing

--- src/pow.cpp
+++ src/pow.cpp
@@ -67,7 +67,11 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         int height = pindexLast->nHeight + 1;
 
         if (height < (Params().WALLET_UPGRADE_BLOCK()+10) && height >= Params().WALLET_UPGRADE_BLOCK())
-            bnNew /= (int)pow(4.0, (double)(10+Params().WALLET_UPGRADE_BLOCK()-height)); // slash difficulty and gradually ramp back up over 10 blocks
+        {
+            //bnNew /= (int)pow(4.0, (double)(10+Params().WALLET_UPGRADE_BLOCK()-height)); // slash difficulty and gradually ramp back up over 10 blocks
+
+            return bnTargetLimit.GetCompact();
+        }
 
         if (bnNew <= 0 || bnNew > bnTargetLimit)
             bnNew = bnTargetLimit;
