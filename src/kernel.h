950aee3fb3c51be28a652c251a6cc4abbdd752c7
apaulb <39216004+apaulb@users.noreply.github.com>
2018-09-08 21:22:11 +0200

    Reduce modifier interval

--- src/kernel.h
+++ src/kernel.h
@@ -10,8 +10,8 @@
 
 
 // MODIFIER_INTERVAL: time to elapse before new modifier is computed
-static const unsigned int MODIFIER_INTERVAL = 3 * 60;
-static const unsigned int MODIFIER_INTERVAL_TESTNET = 3 * 60;
+static const unsigned int MODIFIER_INTERVAL = 5;
+static const unsigned int MODIFIER_INTERVAL_TESTNET = 5;
 extern unsigned int nModifierInterval;
 extern unsigned int getIntervalVersion(bool fTestNet);
 
