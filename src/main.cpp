576d244cdb28b14025cdedaf6372c217dc8752c3
apaulb <39216004+apaulb@users.noreply.github.com>
2018-09-19 18:43:50 +0200

    Check that the block does not overmint

--- src/main.cpp
+++ src/main.cpp
@@ -3103,16 +3103,16 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     LogPrint("bench", "      - Connect %u transactions: %.2fms (%.3fms/tx, %.3fms/txin) [%.2fs]\n", (unsigned)block.vtx.size(), 0.001 * (nTime1 - nTimeStart), 0.001 * (nTime1 - nTimeStart) / block.vtx.size(), nInputs <= 1 ? 0 : 0.001 * (nTime1 - nTimeStart) / (nInputs - 1), nTimeConnect * 0.000001);
 
     //PoW phase redistributed fees to miner. PoS stage destroys fees.
-    CAmount nExpectedMint = GetBlockValue(pindex->pprev->nHeight);
-    if (block.IsProofOfWork())
+    CAmount nExpectedMint = GetBlockValue(pindex->pprev->nHeight, block.IsProofOfStake());
+    if (pindex->nHeight < Params().WALLET_UPGRADE_BLOCK())
         nExpectedMint += nFees;
 
     //Check that the block does not overmint
-/*     if (block.nVersion >= Params().WALLET_UPGRADE_VERSION() && !IsBlockValueValid(block, nExpectedMint, pindex->nMint)) {
+    if (!IsBlockValueValid(block, nExpectedMint, pindex->nMint)) {
         return state.DoS(100, error("ConnectBlock() : reward pays too much (actual=%s vs limit=%s)",
                                     FormatMoney(pindex->nMint), FormatMoney(nExpectedMint)),
                          REJECT_INVALID, "bad-cb-amount");
-    } */
+    }
 
     // Ensure that accumulator checkpoints are valid and in the same state as this instance of the chain
     AccumulatorMap mapAccumulators(Params().Zerocoin_Params(pindex->nHeight < Params().Zerocoin_Block_V2_Start()));
